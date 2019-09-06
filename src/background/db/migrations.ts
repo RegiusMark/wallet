77a4d4ec1452d7e2051d2ea09016db10b9530883
samrg472 <samrg472@gmail.com>
2019-09-06 15:55:21 -0700

    Improve migrations

--- src/background/db/migrations.ts
+++ src/background/db/migrations.ts
@@ -2,27 +2,34 @@ import { WalletDb } from './index';
 import util from 'util';
 
 interface MigrationData {
-  id: number,
-  version: number,
+  id: number;
+  version: number;
 }
 
 export async function runMigrations(db: WalletDb): Promise<void> {
   const migTblName = 'migrations';
-  const tbl = await db.get("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [migTblName]);
-  const exists = tbl !== undefined && tbl.name === migTblName;
-  if (!exists) {
-    await db.run(`CREATE TABLE ${migTblName}(id INTEGER PRIMARY KEY, version INTEGER)`);
-    await db.run(`INSERT INTO ${migTblName}(id, version) VALUES(?, ?)`, [0, 0]);
-  }
 
-  const data: MigrationData = await db.get(`SELECT * FROM ${migTblName}`);
+  const data: MigrationData = await (async () => {
+    const tbl = await db.get("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [migTblName]);
+    const exists = tbl !== undefined && tbl.name === migTblName;
+    if (exists) {
+      return await db.get(`SELECT * FROM ${migTblName}`);
+    } else {
+      return {
+        id: 0,
+        version: 0,
+      };
+    }
+  })();
+
+  console.log('Current database version:', data.version);
+
   switch (data.version) {
     case 0:
       // Upgrade to version 1
-      await db.run(`UPDATE ${migTblName} SET version = $version WHERE id = $id`, {
-        $id: data.id,
-        $version: 1,
-      });
+      await db.run(`CREATE TABLE ${migTblName}(id INTEGER PRIMARY KEY UNIQUE CHECK(id == 0), version INTEGER)`);
+      await db.run(`INSERT INTO ${migTblName}(id, version) VALUES(?, ?)`, [0, 1]);
+      console.log('Upgraded database version to 1');
     /* fall through */
     case 1:
       // Only the last valid case should break for continuous upgrading of the database
