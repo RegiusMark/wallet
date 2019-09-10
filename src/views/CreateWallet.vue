2957634adf74270134853863ad719021e6073ded
samrg472 <samrg472@gmail.com>
2019-09-10 12:29:23 -0700

    Lint fixes

--- src/views/CreateWallet.vue
+++ src/views/CreateWallet.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class CreateWallet extends Vue {
-}
+export default class CreateWallet extends Vue {}
 </script>
--- src/views/RestoreWallet.vue
+++ src/views/RestoreWallet.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class RestoreWallet extends Vue {
-}
+export default class RestoreWallet extends Vue {}
 </script>
