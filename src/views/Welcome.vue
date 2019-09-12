4c6a1b4af522e98539fc2201384aa655136ba58a
samrg472 <samrg472@gmail.com>
2019-09-12 15:33:34 -0700

    Scope the styling on the welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
   <StartArea :bottom-buttons="bottomBtns">
-    <div class="home--welcome-header">{{ isNew ? 'Welcome to GODcoin' : 'Welcome back' }}</div>
+    <div class="welcome-header">{{ isNew ? 'Welcome to GODcoin' : 'Welcome back' }}</div>
     <template v-if="isNew">
-      <div class="home--form">
+      <div class="form">
         <PasswordInput placeholder="Choose your password" />
         <PasswordInput style="padding-top: 40px;" placeholder="Confirm password" />
       </div>
@@ -41,10 +41,10 @@ export default class Welcome extends Vue {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 $btns-color: hsla(0, 0, 100%, 0.5);
 
-.home--welcome-header {
+.welcome-header {
   color: hsla(0, 0, 100%, 0.9);
   text-align: center;
   padding-top: 1.12em;
@@ -53,43 +53,7 @@ $btns-color: hsla(0, 0, 100%, 0.5);
   user-select: none;
 }
 
-.home--form,
-.home--bottom-buttons {
+.form {
   padding-top: 75px;
-}
-
-.home--bottom-buttons {
-  display: flex;
-  justify-content: space-between;
-  text-align: center;
-  user-select: none;
-}
-
-.home--bottom-btn,
-.home--bottom-btn-right {
-  color: $btns-color;
-  border-radius: 4px;
-  padding: 15px;
-  width: 56px;
-  height: 48px;
-
-  .home--bottom-btn-txt {
-    font-size: 0.9em;
-    padding-top: 8px;
-    color: $btns-color;
-  }
-}
-
-.home--bottom-btn {
-  background-color: adjust-color($btns-color, $alpha: -0.44);
-}
-
-.home--bottom-btn-right {
-  background-color: adjust-color($btns-color, $alpha: -0.43);
-  color: adjust-color($btns-color, $alpha: 0.05);
-
-  .home--bottom-btn-txt {
-    font-weight: bold;
-  }
 }
 </style>
