c09ba61addbbc56cfc2476fb2296e9c420502698
samrg472 <samrg472@gmail.com>
2019-10-29 10:40:04 -0700

    Set app icon, code signing identity, and product name

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ module.exports = {
     electronBuilder: {
       mainProcessFile: './src/background/index.ts',
       mainProcessWatch: ['./src/background'],
+      builderOptions: {
+        productName: 'GODcoin Wallet',
+        mac: {
+          identity: 'GODcoin',
+        },
+      },
     },
   },
 };
