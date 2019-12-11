aee9f4d9892ccb0cbc7d0929a13368785395e726
samrg472 <samrg472@gmail.com>
2019-12-11 09:41:51 -0800

    Add dist_electron to transform ignore patterns in Jest

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest',
   },
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['/node_modules/', '/dist_electron/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
