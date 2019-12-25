a0c637bd828ac39cc3d0e19d201e0cc9e6054140
samrg472 <samrg472@gmail.com>
2019-12-24 18:53:03 -0800

    Print console warning when opening dev tools

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,25 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class extends Vue {
+  /* Vue lifecycle hook */
+  private beforeMount(): void {
+    /* eslint-disable no-console */
+    console.log('%cWARNING!', 'background-color: yellow; color: red; font-size: 30px;');
+    console.log(
+      '%cThis is the developer console intended for developers. You must read and understand everything ' +
+        'before pasting or typing it here.',
+      'font-size: 15px;',
+    );
+    /* eslint-enable no-console */
+  }
+}
+</script>
+
 <style lang="scss">
 @import '@/assets/fontawesome/scss/fontawesome.scss';
 @import '@/assets/fontawesome/scss/regular.scss';
